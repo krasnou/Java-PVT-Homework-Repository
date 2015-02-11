e6100049d61706d3411c5d64005fbda4ea498994
krasnou <evgenij.krasnov@gmail.com>
2015-02-11 08:36:50 +0000

    3 zadanie

--- src/Assignment3/Assignment3.java
+++ src/Assignment3/Assignment3.java
@@ -1,7 +1,15 @@
 package Assignment3;
 
 /**
- * Created by jauhien on 2/11/15.
+ * Задача №3. Создайте число. Определите, является ли его последняя цифра семеркой.
  */
 public class Assignment3 {
+    public static void main (String args[]){
+        int number = 357;
+        if(number % 10 == 7){
+            System.out.println("Ends with 7");
+        }else {
+            System.out.println("Not 7");
+        }
+    }
 }
